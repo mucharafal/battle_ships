aa8e1197c33e0d69b4e8d07a9f0a8b28b9438f12
Rafal Mucha <6rafel6@gmail.com>
2019-08-29 22:31:53 +0200

    Modify main to play with bot

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    val player1 = new PlayerAgent()
+    val player1 = new PlayerCLI
     val player2 = new PlayerAgent
 
     val judge = new Judge(player1, player2)
