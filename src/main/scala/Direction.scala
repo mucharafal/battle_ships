dd4f269435ff38bc9537d441955d5b721405ab02
Rafal Mucha <6rafel6@gmail.com>
2019-08-31 09:10:30 +0200

    Add missed in git files

---
+++ src/main/scala/Direction.scala
@@ -0,0 +1,4 @@
+object Direction extends Enumeration {
+  type Direction = Value
+  val Horizontal, Vertical = Value
+}
---
+++ src/main/scala/Ship.scala
@@ -0,0 +1,37 @@
+import Direction._
+
+
+case class Ship(length: Int,
+                positionX: Int, positionY: Int, direction: Direction) {
+  def collide(other: Ship): Boolean= {
+    direction match {
+      case Vertical =>
+        (0 until length).exists(x => {other.isIn(x + positionX, positionY)})
+      case Horizontal =>
+        (0 until length).exists(x => {other.isIn(positionX, positionY + x)})
+    }
+  }
+
+  def isIn(pointX: Int, pointY: Int): Boolean = {
+    direction match {
+      case Vertical =>
+        positionY == pointY && positionX <= pointX && positionX + length > pointX
+      case Horizontal =>
+        positionX == pointX && positionY <= pointY && positionY + length > pointY
+    }
+  }
+
+  def getListOfFieldsCooridinates: List[(Int, Int)] = {
+    var coordinates: List[(Int, Int)] = List()
+    for(i <- 0 until length) {
+      direction match {
+        case Vertical => coordinates = (positionX + i, positionY) :: coordinates
+        case Horizontal => coordinates = (positionX, positionY + i) :: coordinates
+      }
+    }
+    coordinates
+  }
+
+
+}
+
